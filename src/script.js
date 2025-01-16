556cd3fe4f4f5d9a70ad5d0353265228b1a66b3d
JeanRauta <j34nwr@gmail.com>
2025-01-16 12:35:02 -0300

    up

--- src/script.js
+++ src/script.js
@@ -4,10 +4,19 @@ document.querySelector('.about').addEventListener('click', function() {
 document.querySelector('.works').addEventListener('click', function() {
     scrollar(document.querySelector('.worksFlag'));
 });
-document.querySelector('.resume').addEventListener('click', function() {
-    scrollar(document.querySelector('.resumeFlag'));
-});
 
 function scrollar(elemento) {
     elemento.scrollIntoView({ behavior: "smooth" });
+}
+
+function img01() {
+    window.open("https://github.com/JeanRauta/chordinoAPI", "_blank");
+}
+
+function img02() {
+    window.open("https://comebem.onrender.com/", "_blank");
+}
+
+function img03() {
+    window.open("https://github.com/JeanRauta/chatapi.git", "_blank");
 }
